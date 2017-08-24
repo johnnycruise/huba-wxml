8cd621a15701c81bd252786ee158ddd2e9c3e7e3
johnnycruise <johnnyhuang86@hotmail.com>
2017-08-24 17:59:38 +0800

    v1.0.0

---
+++ pages/index/index-model.js
@@ -0,0 +1,18 @@
+// index-model.js
+
+class Home{
+    constructor(){
+
+    }
+    getBannerData(id){
+        wx.request({
+            url:'http://z.cn/api/v1/banner/' + id,
+            method:'GET'
+            success:function(res){
+                return res;
+            }
+        })
+    }
+}
+
+export{Home};
---
+++ pages/index/index.js
@@ -0,0 +1,18 @@
+// index.js
+import {Home} from 'home-model.js';
+var home = new Home();
+Page({
+    data:{
+
+    },
+
+    onLoad:function(){
+        this._loadData();
+    },
+
+    _loadData:function(){
+        var id = 1;
+        var data = home.getBannerData();
+
+    }
+})
