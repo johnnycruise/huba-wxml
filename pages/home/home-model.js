da490c6378cff7eaebfb144b922cf8b4269a6a15
johnnycruise <johnnyhuang86@hotmail.com>
2017-08-25 10:03:08 +0800

    v1.0.0

---
+++ pages/home/home-model.js
@@ -0,0 +1,20 @@
+// index-model.js
+
+class Home{
+    
+    constructor(){
+
+    }
+
+    getBannerData(id){
+        wx.request({
+            url:'http://z.cn/api/v1/banner/' + id,
+            method:'GET'
+            success:function(res){
+                return res;
+            }
+        })
+    }
+}
+
+export{Home};
